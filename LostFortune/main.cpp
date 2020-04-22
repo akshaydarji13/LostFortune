78a37a71564c36150be9084d705e6ab946d332ec
Akshay Darji <akshay7694@live.com>
2020-04-22 22:31:57 +0530

    Lost fortune main file

--- LostFortune/main.cpp
+++ LostFortune/main.cpp
@@ -1,15 +1,57 @@
 //
 //  main.cpp
 //  LostFortune
+//  A personalized adventure
 //
 //  Created by Akshay Darji on 22/04/20.
 //  Copyright © 2020 Akshay Darji. All rights reserved.
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+  const int GOLD_PIECES = 900;
+  int adventurers, killed, survivors;
+  string leader;
+  
+  //  get the information
+  cout << "Welcome to Lost Fortune\n\n";
+  cout << "Please enter the following for your personalized adventure\n";
+  
+  cout << "Enter a number: ";
+  cin >> adventurers;
+  
+  cout << "Enter a number, smaller than the first: ";
+  cin >> killed;
+  
+  survivors = adventurers - killed;
+  
+  cout << "Enter your name: ";
+  cin >> leader;
+  
+  //  tell the story
+  cout << "\nA brave group of " << adventurers << " set out on a quest ";
+  cout << "-- in search of the lost treasure of the Ancient Dwarves. ";
+  cout << "The group was led by that legendary rogue, " << leader << ".\n";
+  
+  cout << "\nAlong the way, a band of marauding ogres ambushed the party. ";
+  cout << "All fought bravely under the command of " << leader;
+  cout << ", and the ogres were defeated, but at a cost. ";
+  cout << "Of the adventurers, " << killed << " were vanquished, ";
+  cout << "leaving just " << survivors << " in the group.\n";
+  
+  cout << "\nThe party was about to give up all hope. ";
+  cout << "But while laying the deceased to rest, ";
+  cout << "they stumbled upon the buried fortune. ";
+  cout << "So the adventurers split " << GOLD_PIECES << " gold pieces.";
+  cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+  cout << " pieces to keep things fair of course.\n";
+  
+  return 0;
 }
