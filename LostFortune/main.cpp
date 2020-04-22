63b92addc7099993e5d44293a804fd6a5a2cc504
Akshay Darji <akshay7694@live.com>
2020-04-22 17:42:07 +0530

    Initial Commit

---
+++ LostFortune/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LostFortune
+//
+//  Created by Akshay Darji on 22/04/20.
+//  Copyright © 2020 Akshay Darji. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
